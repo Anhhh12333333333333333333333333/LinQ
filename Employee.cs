841fb0161e714d069d97aa1afa4a3e5304678b3e
Anhhh12333333333333333333333333 <hodangquocanh2003@gmail.com>
2024-03-11 18:49:41 +0700

    first commit

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public DateTime birthday { get; set; }
+        public int DepartmentID { get; set; }
+        public static IList<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+            {
+                new Employee{ ID=1, Name="A", Age=21, Salary=20000000, birthday= DateTime.Parse("2002-11-11"), DepartmentID=1},
+                new Employee{ ID=2, Name="B", Age=31, Salary=30000000, birthday= DateTime.Parse("2003-9-11"), DepartmentID=2},
+                new Employee{ ID=3, Name="C", Age=41, Salary=10000000, birthday= DateTime.Parse("2001-10-11"), DepartmentID=3},
+                new Employee{ ID=4, Name="D", Age=35, Salary=50000000, birthday= DateTime.Parse("2002-1-11"), DepartmentID=1},
+                new Employee{ ID=5, Name="E", Age=30, Salary=15000000, birthday= DateTime.Parse("2002-11-11"), DepartmentID=2}
+
+
+
+
+            };
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lstDep = Department.GetDepartments();
+            var lstEmp = Employee.GetEmployees();
+
+            var Maxsalary = lstEmp.Max(p => p.Salary);
+            var MinSalary = lstEmp.Min(p => p.Salary);
+            var TBCSalary = lstEmp.Average(p => p.Salary);
+            Console.WriteLine("Ho Dang Quoc Anh-21115053120304");
+
+            Console.WriteLine("Câu a");
+            Console.WriteLine("Max salary: {0}", Maxsalary);
+            Console.WriteLine("Min salary: {0}", MinSalary);
+            Console.WriteLine("Average salary: {0}", TBCSalary);
+
+
+            Console.WriteLine("Câu b");
+            Console.WriteLine("Group Join");
+
+            var grJoin = lstDep.GroupJoin(lstEmp,
+                n => n.ID,
+                p => p.DepartmentID,
+                (n, p) => new
+                {
+                    Department = n,
+                    Employee = p.ToList()
+                }
+                );
+            foreach (var item in grJoin)
+            {
+                Console.WriteLine($"Department: {item.Department.Name}");
+                foreach (var emp in item.Employee)
+                {
+                    Console.WriteLine($"- {emp.Name}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Left Join");
+
+            var departmentEmployees = from dept in lstDep
+                                      join emp in lstEmp on dept.ID equals emp.DepartmentID into empGroup
+                                      from employee in empGroup.DefaultIfEmpty()
+                                      select new
+                                      {
+                                          Department = dept,
+                                          Employee = employee
+                                      };
+
+            // In ra thông tin của từng Department và Employee tương ứng (nếu có)
+            foreach (var item in departmentEmployees)
+            {
+                Console.WriteLine($"Department: {item.Department.Name}, Employee: {(item.Employee != null ? item.Employee.Name : "No employee")}");
+            }
+            Console.WriteLine("Right Join");
+
+            var rightjoin = from emp in lstEmp
+                            join dept in lstDep on emp.DepartmentID equals dept.ID into deptGroup
+                            from department in deptGroup.DefaultIfEmpty()
+                            select new
+                            {
+                                Department = department,
+                                Employee = emp
+                            };
+
+            foreach (var item in rightjoin)
+            {
+                Console.WriteLine($"Department: {(item.Department != null ? item.Department.Name : "No department")}, Employee: {item.Employee.Name}");
+            }
+            Console.WriteLine("Câu c");
+
+            int maxAge = lstEmp.Max(e => CalculateAge(e.birthday));
+            int minAge = lstEmp.Min(e => CalculateAge(e.birthday));
+
+            Console.WriteLine($"Max Age: {maxAge}");
+            Console.WriteLine($"Min Age: {minAge}");
+
+            int CalculateAge ( DateTime bt)
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - bt.Year;
+                if (bt.Date > today.AddYears(-age))
+                    age--;
+
+                return age;
+            }
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
